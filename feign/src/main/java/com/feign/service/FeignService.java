9095aa0e82c8dda7a353e9f34344b3acf09348ca
FaceFeel <995956062@qq.com>
2018-05-24 14:09:18 +0800

    1.为ribbon和feign添加熔断机制

--- feign/src/main/java/com/feign/service/FeignService.java
+++ feign/src/main/java/com/feign/service/FeignService.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * FeignClient注解中的值表示的是要访问/请求的目标服务的名称
+ * fallback说明:该参数的指定熔断机制的处理实现.该参数中的类是本接口(实现熔断的接口)
+ * 的实现类.当sayHiFromClientOne方法访问远程服务发生错误时,则会自动执行本接口的实现类下的
+ * 实现方法,并返回该方法中的内容.
  * @author FaceFeel
  * @Created 2018-05-23 17:05
  **/
-@FeignClient(value="service-hello")
+@FeignClient(value="service-hello",fallback = FeignServiceImpl.class)
 public interface FeignService {
 
     /**
--- ribbon/src/main/java/com/ribbon/service/HelloServiceImpl.java
+++ ribbon/src/main/java/com/ribbon/service/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,14 +16,27 @@ public class HelloServiceImpl implements HelloService {
     private RestTemplate restTemplate;
 
     /**
+     * 第一个注解表示的是:为该方法创建了熔断机制/功能
+     * 请求远程服务方法
      *
-     * @param name
-     * @return
+     * @param name 传入参数
+     * @return ""
      */
+    @HystrixCommand(fallbackMethod = "hiError")
     @Override
     public String hiService(String name) {
 
 //        return restTemplate.getForObject("http://SERVICE-HELLO/hello?name=" + name, String.class);
         return restTemplate.getForObject("http://SERVICE-HELLO/hi?name=" + name, String.class);
+    }
+
+    /**
+     * 该方法是hiService方法上的@HystrixCommand(fallbackMethod = "hiError")
+     * 注解中的回调方法,这是熔断的处理机制
+     * @param name ""
+     * @return ""
+     */
+    public String hiError(String name) {
+        return "hi " + name + ",sorry, error !";
     }
 }
