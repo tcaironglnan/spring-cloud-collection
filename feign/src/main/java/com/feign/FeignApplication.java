6640d1973768a452e6e16c00cf0a531bad6e2c11
FaceFeel <995956062@qq.com>
2018-05-24 16:36:33 +0800

    1.为ribbon和feign添加仪表盘

--- feign/src/main/java/com/feign/FeignApplication.java
+++ feign/src/main/java/com/feign/FeignApplication.java
@@ -2,12 +2,18 @@ package com.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class FeignApplication {
 
     public static void main(String[] args) {
