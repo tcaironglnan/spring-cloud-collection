8e3fbf08e01c82d6756bd9b6dab864317cb11036
FaceFeel <995956062@qq.com>
2018-05-25 17:22:55 +0800

    1.fix

--- zipkin/src/main/java/com/zipkin/ZipkinApplication.java
+++ zipkin/src/main/java/com/zipkin/ZipkinApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableZipkinServ1er
+@EnableZipkinServer
 public class ZipkinApplication {
 
     public static void main(String[] args) {
