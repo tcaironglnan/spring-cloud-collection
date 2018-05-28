714b23abff5535c7ad8826dc88df3f97da69b5b3
tcaironglnan <995956062@qq.com>
2018-05-28 23:45:07 +0800

    1.fix

--- zipkin-client/src/main/java/com/client/ZipkinClientApplication.java
+++ zipkin-client/src/main/java/com/client/ZipkinClientApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClientApplication {
+public class ZipkinClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(ZipkinClientApplication.class, args);
     }
 }
