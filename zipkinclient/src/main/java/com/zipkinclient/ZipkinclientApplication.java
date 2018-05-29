d413b1fa6c830d562959d9882c59ec72fd0eab36
FaceFeel <995956062@qq.com>
2018-05-29 10:35:23 +0800

    1.fix

--- zipkinclient/src/main/java/com/zipkinclient/ZipkinclientApplication.java
+++ zipkinclient/src/main/java/com/zipkinclient/ZipkinclientApplication.java
@@ -1,4 +1,4 @@
-package com.client;
+package com.zipkinclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class ZipkinClientApplication {
+public class ZipkinclientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZipkinClientApplication.class, args);
+        SpringApplication.run(ZipkinclientApplication.class, args);
     }
 
     @Bean
